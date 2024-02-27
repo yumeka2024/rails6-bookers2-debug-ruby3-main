a2cd98bcca49b732e1ee48da67ff86f28fa4f8a4
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-02-27 07:02:13 +0000

    [Add]Bootstrapのyarn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books.
+    @books = @user.books.all
     @book = Book.new
   end
 
@@ -13,13 +13,15 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
-      render "show"
+      render :edit
     end
   end
 
