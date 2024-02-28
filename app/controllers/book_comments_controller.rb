8d0409e9edeb20a369c5cb61ff77ade73f4b0147
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-02-28 09:32:24 +0000

    [Add]コメント機能(View)

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,26 +1,27 @@
 class BookCommentsController < ApplicationController
   before_action :is_matching_login_user, only: [:destroy]
-  
+
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book.new(book_comment_params)
+    comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
-    Book.find(params[:book_id]).destroy
+    book = Book.find(params[:book_id])
+    BookComment.find(params[:id]).destroy
     redirect_to book_path(book.id)
   end
-  
-  
+
+
   private
 
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
+
   #ログインユーザーと一致しないとアクセスさせない
   def is_matching_login_user
     book = Book.find(params[:id])
@@ -29,5 +30,5 @@ class BookCommentsController < ApplicationController
       redirect_to books_path
     end
   end
-  
+
 end
