cffb2e4213f5a7d3251030d436f391997758dce0
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-02-28 08:12:13 +0000

    [Add]いいね機能(View)

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,12 @@
 class Book < ApplicationRecord
   belongs_to :user, optional:true
   has_many :favorites, dependent: :destroy
-  
+
   validates :title, presence:true
   validates :body, presence:true, length:{maximum:200}
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
