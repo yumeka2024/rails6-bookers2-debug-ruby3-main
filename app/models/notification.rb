ad138d596ad9aa1f1913084a234e603695582fc9
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-03-31 16:08:55 +0000

    [Add]未読⇒既読処理(未完)

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,24 @@
 class Notification < ApplicationRecord
+  include Rails.application.routes.url_helpers
+  # モデルの中でxxx_pathメソッドを使用するために必要な記述になります。
+
   belongs_to :user
   belongs_to :notifiable, polymorphic: true
+
+  def message
+    if notifiable_type === "Book"
+      "フォローしている#{notifiable.user.name}さんが#{notifiable.title}を投稿しました"
+    else
+      "投稿した#{notifiable.book.title}が#{notifiable.user.name}さんにいいねされました"
+    end
+  end
+
+  def notifiable_path
+    if notification.notifiable_type === "Book"
+      book_path(notifiable.id)
+    else
+      user_path(notifiable.user.id)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  
+
   # get 'searches/search'
   # get 'relationships/follower'
   # get 'relationships/followed'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
@@ -14,25 +14,27 @@ Rails.application.routes.draw do
    resource :favorite, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
   end
-  
+
   # resources :users, only: [:index,:show,:edit,:update] do
   #   resource :relationship, only: [:create, :destroy]
   #   get 'follower' => 'relationships#follower', as: 'follower'
   #   get 'followed' => 'relationships#followed', as: 'followed'
   # end
-  
+
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
   	get "followings" => "relationships#followings", as: "followings"
   	get "followers" => "relationships#followers", as: "followers"
   end
-  
+
   get "/search", to: "searches#search"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
+
+  resources :notifications, only: [:update]
 
 end
